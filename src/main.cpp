1060d86d6ee82d6789d2e13c62aa6573d4f2b7e9
Adam Florin <hey@adamflorin.work>
2018-04-10 21:05:40 -0700

    Add second oscillator

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@
 #include <SerialFlash.h>
 
 AudioSynthWaveform waveform1;
+AudioSynthWaveform waveform2;
+AudioMixer4 mixer;
 AudioEffectEnvelope envelope1;
 AudioOutputI2S i2s1;
-AudioConnection patchCord1(waveform1, envelope1);
-AudioConnection patchCord2(envelope1, 0, i2s1, 0);
-AudioConnection patchCord3(envelope1, 0, i2s1, 1);
+AudioConnection patchCord1(waveform1, 0, mixer, 0);
+AudioConnection patchCord2(waveform2, 0, mixer, 1);
+AudioConnection patchCord3(mixer, envelope1);
+AudioConnection patchCord4(envelope1, 0, i2s1, 0);
+AudioConnection patchCord5(envelope1, 0, i2s1, 1);
 AudioControlSGTL5000 sgtl5000_1;
 
 void setup() {
@@ -19,10 +23,19 @@ void setup() {
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.8);
 
-  // oscillator
+  // oscillator - sine
   waveform1.frequency(220.0);
   waveform1.amplitude(0.9);
   waveform1.begin(WAVEFORM_SINE);
+
+  // oscillator - saw
+  waveform2.frequency(220.0 * 0.997);
+  waveform2.amplitude(0.3);
+  waveform2.begin(WAVEFORM_SAWTOOTH);
+
+  // mixer
+  mixer.gain(0, 0.5);
+  mixer.gain(1, 0.5);
 
   // envelope
   envelope1.attack(100);
