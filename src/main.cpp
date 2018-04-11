68a0a63e62f5929848a3efffaa49e6fc9a4b4155
Adam Florin <hey@adamflorin.work>
2018-04-10 20:52:42 -0700

    Add envelope

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,41 @@
 #include <SPI.h>
 #include <SerialFlash.h>
 
-AudioSynthWaveform    waveform1;
-AudioOutputI2S        i2s1;
-AudioConnection       patchCord1(waveform1, 0, i2s1, 0);
-AudioConnection       patchCord2(waveform1, 0, i2s1, 1);
-AudioControlSGTL5000  sgtl5000_1;
+AudioSynthWaveform waveform1;
+AudioEffectEnvelope envelope1;
+AudioOutputI2S i2s1;
+AudioConnection patchCord1(waveform1, envelope1);
+AudioConnection patchCord2(envelope1, 0, i2s1, 0);
+AudioConnection patchCord3(envelope1, 0, i2s1, 1);
+AudioControlSGTL5000 sgtl5000_1;
 
 void setup() {
   AudioMemory(10);
 
-  Serial.begin(115200);
-
+  // output
   sgtl5000_1.enable();
+  sgtl5000_1.volume(0.8);
 
-  sgtl5000_1.volume(0.3);
-
+  // oscillator
+  waveform1.frequency(220.0);
+  waveform1.amplitude(0.9);
   waveform1.begin(WAVEFORM_SINE);
+
+  // envelope
+  envelope1.attack(100);
+  envelope1.decay(100);
+  envelope1.sustain(0.9);
+  envelope1.release(900);
 
   delay(1000);
 }
 
 void loop() {
-  waveform1.frequency(1280);
-  waveform1.amplitude(0.9);
+  // on
+  envelope1.noteOn();
   delay(250);
 
-  waveform1.amplitude(0);
+  // off
+  envelope1.noteOff();
   delay(1750);
 }
