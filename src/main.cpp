757a258cf9ce4642894ea653b4a82ddbf3198a15
Adam Florin <hey@adamflorin.work>
2018-04-12 15:43:02 -0700

    Emulate Oscs 1 & 2 w/ 4 spread voices each

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,42 @@
 #include <SPI.h>
 #include <SerialFlash.h>
 
-AudioSynthWaveform osc1;
-// AudioSynthWaveform osc2;
+AudioSynthWaveform osc1a;
+AudioSynthWaveform osc1b;
+AudioSynthWaveform osc1c;
+AudioSynthWaveform osc1d;
+AudioSynthWaveform osc2a;
+AudioSynthWaveform osc2b;
+AudioSynthWaveform osc2c;
+AudioSynthWaveform osc2d;
+AudioMixer4 osc1mixer;
+AudioMixer4 osc2mixer;
 AudioMixer4 mixer;
 AudioEffectEnvelope envelope;
-AudioOutputI2S fakeOutput;
-AudioOutputUSB output;
-AudioConnection patchCord1(osc1, 0, mixer, 0);
-// AudioConnection patchCord2(osc2, 0, mixer, 1);
-AudioConnection patchCord3(mixer, envelope);
-AudioConnection patchCord5(envelope, 0, output, 0);
-AudioConnection patchCord6(envelope, 0, output, 1);
+AudioOutputI2S headphoneOutput;
+AudioOutputUSB usbOutput;
+AudioConnection patchCord1(osc1a, 0, osc1mixer, 0);
+AudioConnection patchCord2(osc1b, 0, osc1mixer, 1);
+AudioConnection patchCord3(osc1c, 0, osc1mixer, 2);
+AudioConnection patchCord4(osc1d, 0, osc1mixer, 3);
+AudioConnection patchCord5(osc2a, 0, osc2mixer, 0);
+AudioConnection patchCord6(osc2b, 0, osc2mixer, 1);
+AudioConnection patchCord7(osc2c, 0, osc2mixer, 2);
+AudioConnection patchCord8(osc2d, 0, osc2mixer, 3);
+AudioConnection patchCord9(osc1mixer, 0, mixer, 0);
+AudioConnection patchCord10(osc2mixer, 0, mixer, 1);
+AudioConnection patchCord11(mixer, envelope);
+AudioConnection patchCord12(envelope, 0, headphoneOutput, 0);
+AudioConnection patchCord13(envelope, 0, headphoneOutput, 1);
 AudioControlSGTL5000 sgtl5000_1;
 
 void setup() {
+  float spread_cents = 3.0;
+  float spread_up = powf(2.0, spread_cents / 1200.0);
+  float spread_down = powf(2.0, -spread_cents / 1200.0);
+  float spread_up_half = powf(2.0, spread_cents / 2.0 / 1200.0);
+  float spread_down_half = powf(2.0, -spread_cents / 2.0 / 1200.0);
+
   AudioMemory(10);
 
   // output
@@ -25,19 +47,62 @@ void setup() {
   sgtl5000_1.volume(0.15);
 
   // Osc 1
-  osc1.frequency(110.0); // A2
-  osc1.sync(4.0);
-  osc1.amplitude(0.9);
-  osc1.begin(WAVEFORM_SQUARE_HARD_SYNC);
+  float osc1_base_freq = 110.0; // A3
+  osc1a.frequency(osc1_base_freq * spread_up);
+  osc1a.sync(4.0);
+  osc1a.amplitude(1.0);
+  osc1a.phase(0.0);
+  osc1a.begin(WAVEFORM_SQUARE);
+  osc1b.frequency(osc1_base_freq * spread_down);
+  osc1b.sync(4.0);
+  osc1b.amplitude(1.0);
+  osc1b.phase(45.0);
+  osc1b.begin(WAVEFORM_SQUARE);
+  osc1c.frequency(osc1_base_freq * spread_up_half);
+  osc1c.sync(4.0);
+  osc1c.amplitude(1.0);
+  osc1c.phase(90.0);
+  osc1c.begin(WAVEFORM_SQUARE);
+  osc1d.frequency(osc1_base_freq * spread_down_half);
+  osc1d.sync(4.0);
+  osc1d.amplitude(1.0);
+  osc1d.phase(135.0);
+  osc1d.begin(WAVEFORM_SQUARE);
 
-  // // oscillator - saw
-  // osc2.frequency(220.0 * 0.9997);
-  // osc2.amplitude(0.3);
-  // osc2.begin(WAVEFORM_SAWTOOTH);
+  // Osc 2
+  float osc2_base_freq = 440.0 * powf(2, 10.9 / 1200.0);
+  osc2a.frequency(osc2_base_freq * spread_up);
+  osc2a.sync(1.476);
+  osc2a.amplitude(1.0);
+  osc2a.phase(0.0);
+  osc2a.begin(WAVEFORM_SAWTOOTH_REVERSE);
+  osc2b.frequency(osc2_base_freq * spread_down);
+  osc2b.sync(1.476);
+  osc2b.amplitude(1.0);
+  osc2b.phase(45.0);
+  osc2b.begin(WAVEFORM_SAWTOOTH_REVERSE);
+  osc2c.frequency(osc2_base_freq * spread_up_half);
+  osc2c.sync(1.476);
+  osc2c.amplitude(1.0);
+  osc2c.phase(90.0);
+  osc2c.begin(WAVEFORM_SAWTOOTH_REVERSE);
+  osc2d.frequency(osc2_base_freq * spread_down_half);
+  osc2d.sync(1.476);
+  osc2d.amplitude(1.0);
+  osc2d.phase(135.0);
+  osc2d.begin(WAVEFORM_SAWTOOTH_REVERSE);
 
-  // mixer
-  mixer.gain(0, 0.5);
-  // mixer.gain(1, 0.5);
+  // mixers
+  osc1mixer.gain(0, 0.5);
+  osc1mixer.gain(1, 0.5);
+  osc1mixer.gain(2, 0.5);
+  osc1mixer.gain(3, 0.5);
+  osc2mixer.gain(0, 0.5);
+  osc2mixer.gain(1, 0.5);
+  osc2mixer.gain(2, 0.5);
+  osc2mixer.gain(3, 0.5);
+  mixer.gain(0, 0.85); // Osc 1: eyeballing -0.61 dB
+  mixer.gain(1, 0.9); // Osc 2: eyeballing 0 dB
 
   // envelope
   envelope.attack(10.0);
