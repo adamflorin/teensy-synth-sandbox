3cb82729a532eabcec5aaf37c3d5e1009ee6386b
Adam Florin <hey@adamflorin.work>
2018-04-12 12:41:28 -0700

    Emulate Osc 1 - over USB audio

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,17 @@
 #include <SPI.h>
 #include <SerialFlash.h>
 
-AudioSynthWaveform waveform1;
-AudioSynthWaveform waveform2;
+AudioSynthWaveform osc1;
+// AudioSynthWaveform osc2;
 AudioMixer4 mixer;
-AudioEffectEnvelope envelopeL;
-AudioEffectEnvelope envelopeR;
-AudioOutputI2S i2s1;
-AudioConnection patchCord1(waveform1, 0, mixer, 0);
-AudioConnection patchCord2(waveform2, 0, mixer, 1);
-AudioConnection patchCord3(mixer, envelopeL);
-AudioConnection patchCord4(mixer, envelopeR);
-AudioConnection patchCord5(envelopeL, 0, i2s1, 0);
-AudioConnection patchCord6(envelopeR, 0, i2s1, 1);
+AudioEffectEnvelope envelope;
+AudioOutputI2S fakeOutput;
+AudioOutputUSB output;
+AudioConnection patchCord1(osc1, 0, mixer, 0);
+// AudioConnection patchCord2(osc2, 0, mixer, 1);
+AudioConnection patchCord3(mixer, envelope);
+AudioConnection patchCord5(envelope, 0, output, 0);
+AudioConnection patchCord6(envelope, 0, output, 1);
 AudioControlSGTL5000 sgtl5000_1;
 
 void setup() {
@@ -25,42 +24,34 @@ void setup() {
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.15);
 
-  // oscillator - sine
-  waveform1.frequency(110.0);
-  waveform1.amplitude(0.9);
-  waveform1.begin(WAVEFORM_SINE);
+  // Osc 1
+  osc1.frequency(110.0); // A2
+  osc1.sync(4.0);
+  osc1.amplitude(0.9);
+  osc1.begin(WAVEFORM_SQUARE_HARD_SYNC);
 
-  // oscillator - saw
-  waveform2.frequency(220.0 * 0.9997);
-  waveform2.amplitude(0.3);
-  waveform2.begin(WAVEFORM_SAWTOOTH);
+  // // oscillator - saw
+  // osc2.frequency(220.0 * 0.9997);
+  // osc2.amplitude(0.3);
+  // osc2.begin(WAVEFORM_SAWTOOTH);
 
   // mixer
   mixer.gain(0, 0.5);
-  mixer.gain(1, 0.5);
+  // mixer.gain(1, 0.5);
 
   // envelope
-  envelopeL.attack(500);
-  envelopeL.decay(100);
-  envelopeL.sustain(0.9);
-  envelopeL.release(1000);
-
-  envelopeR.attack(500);
-  envelopeR.decay(100);
-  envelopeR.sustain(0.9);
-  envelopeR.release(1000);
+  envelope.attack(10.0);
+  envelope.decay(2144.0);
+  envelope.sustain(0.6); // eyeballing -3.54 dB
+  envelope.hold(0.0);
+  envelope.release(1076.0);
 
   delay(1000);
 }
 
 void loop() {
-  envelopeL.noteOn();
+  envelope.noteOn();
   delay(500);
-  envelopeL.noteOff();
-  delay(500);
-
-  envelopeR.noteOn();
-  delay(500);
-  envelopeR.noteOff();
-  delay(500);
+  envelope.noteOff();
+  delay(1500);
 }
