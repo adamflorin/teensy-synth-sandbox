ddcd6529ab7d6206167f173e2de0d06a9938ac29
Adam Florin <hey@adamflorin.work>
2018-04-17 10:45:18 -0700

    Use SQUARETOOTH; adjust gains, spread, phase

--- src/main.cpp
+++ src/main.cpp
@@ -38,62 +38,57 @@ AudioConnection patchCord13(envelope, 0, usbOutput, 1);
 AudioControlSGTL5000 sgtl5000_1;
 
 void setup() {
+  float num_voices = 4.0;
   float spread_cents = 3.0;
-  float spread_up = powf(2.0, spread_cents / 1200.0);
-  float spread_down = powf(2.0, -spread_cents / 1200.0);
-  float spread_up_half = powf(2.0, spread_cents / 2.0 / 1200.0);
-  float spread_down_half = powf(2.0, -spread_cents / 2.0 / 1200.0);
+  float spread_increment = (spread_cents * 2.0) / (num_voices - 1.0);
+  const float CENTS_PER_OCTAVE = 1200.0;
+  float spread_up = powf(2.0, spread_cents / CENTS_PER_OCTAVE);
+  float spread_down = powf(2.0, -spread_cents / CENTS_PER_OCTAVE);
+  float spread_up_half = powf(2.0, (spread_cents - spread_increment) / CENTS_PER_OCTAVE);
+  float spread_down_half = powf(2.0, (-spread_cents + spread_increment) / CENTS_PER_OCTAVE);
 
   AudioMemory(50);
 
   // output
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.15);
+  sgtl5000_1.volume(1.0);
 
   // Osc 1
   float osc1_base_freq = 110.0; // A3
   osc1a.frequency(osc1_base_freq * spread_up);
   osc1a.sync(4.0);
   osc1a.amplitude(1.0);
-  osc1a.phase(0.0);
-  osc1a.begin(WAVEFORM_SQUARE);
+  osc1a.begin(WAVEFORM_SQUARETOOTH);
   osc1b.frequency(osc1_base_freq * spread_down);
   osc1b.sync(4.0);
   osc1b.amplitude(1.0);
-  osc1b.phase(45.0);
-  osc1b.begin(WAVEFORM_SQUARE);
+  osc1b.begin(WAVEFORM_SQUARETOOTH);
   osc1c.frequency(osc1_base_freq * spread_up_half);
   osc1c.sync(4.0);
   osc1c.amplitude(1.0);
-  osc1c.phase(90.0);
-  osc1c.begin(WAVEFORM_SQUARE);
+  osc1c.begin(WAVEFORM_SQUARETOOTH);
   osc1d.frequency(osc1_base_freq * spread_down_half);
   osc1d.sync(4.0);
   osc1d.amplitude(1.0);
-  osc1d.phase(135.0);
-  osc1d.begin(WAVEFORM_SQUARE);
+  osc1d.begin(WAVEFORM_SQUARETOOTH);
 
   // Osc 2
-  float osc2_base_freq = 440.0 * powf(2, 10.9 / 1200.0);
+  float osc2_base_freq = 4.0 * osc1_base_freq * powf(2, 10.9 / 1200.0);
   osc2a.frequency(osc2_base_freq * spread_up);
   osc2a.sync(1.476);
   osc2a.amplitude(1.0);
-  osc2a.phase(0.0);
   osc2a.begin(WAVEFORM_SAWTOOTH_REVERSE);
   osc2b.frequency(osc2_base_freq * spread_down);
   osc2b.sync(1.476);
   osc2b.amplitude(1.0);
-  osc2b.phase(45.0);
   osc2b.begin(WAVEFORM_SAWTOOTH_REVERSE);
   osc2c.frequency(osc2_base_freq * spread_up_half);
   osc2c.sync(1.476);
   osc2c.amplitude(1.0);
-  osc2c.phase(90.0);
   osc2c.begin(WAVEFORM_SAWTOOTH_REVERSE);
   osc2d.frequency(osc2_base_freq * spread_down_half);
   osc2d.sync(1.476);
   osc2d.amplitude(1.0);
-  osc2d.phase(135.0);
   osc2d.begin(WAVEFORM_SAWTOOTH_REVERSE);
 
   // filters
@@ -106,16 +101,16 @@ void setup() {
   osc2filter.setLowpass(0, filter2freq, filter2q);
 
   // mixers
-  osc1mixer.gain(0, 0.5);
-  osc1mixer.gain(1, 0.5);
-  osc1mixer.gain(2, 0.5);
-  osc1mixer.gain(3, 0.5);
-  osc2mixer.gain(0, 0.5);
-  osc2mixer.gain(1, 0.5);
-  osc2mixer.gain(2, 0.5);
-  osc2mixer.gain(3, 0.5);
-  mixer.gain(0, 0.48); // Osc 1: eyeballing -0.61 dB
-  mixer.gain(1, 0.5); // Osc 2: eyeballing 0 dB
+  osc1mixer.gain(0, 0.15);
+  osc1mixer.gain(1, 0.15);
+  osc1mixer.gain(2, 0.15);
+  osc1mixer.gain(3, 0.15);
+  osc2mixer.gain(0, 0.15);
+  osc2mixer.gain(1, 0.15);
+  osc2mixer.gain(2, 0.15);
+  osc2mixer.gain(3, 0.15);
+  mixer.gain(0, 0.9); // Osc 1: eyeballing -0.61 dB
+  mixer.gain(1, 1.0); // TEMP // Osc 2: eyeballing 0 dB
 
   // envelope
   envelope.attack(10.0);
